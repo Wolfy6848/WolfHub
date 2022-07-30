b5a207c37a715b8fc35efa75bbbe06bdfe3bbaf9
Wolfy6848 <89296427+Wolfy6848@users.noreply.github.com>
2022-07-30 14:56:58 +0100

    Create Main..lua

---
+++ Main..lua
@@ -0,0 +1,11 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "WolfHub"})
+
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	PremiumOnly = True
+})
+
+
+OrionLib:Init()
